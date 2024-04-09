808ee5c987b522ebc6e1b6fe1bc1c960ea928d1d
Maria Shmakova <masha.shmakova@gmail.com>
2024-04-09 16:14:21 +0300

    react goods selector corrected

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,68 @@ export const goods = [
   'Garlic',
 ];
 
+const SelectedGood = ({ selectedGood, reset }) => (
+  <h1 className="title is-flex is-align-items-center">
+    {selectedGood} is selected
+    <button
+      data-cy="ClearButton"
+      type="button"
+      className="delete ml-3"
+      onClick={reset}
+    />
+  </h1>
+);
+
+const Table = ({ selectedGood, setSelectedGood, reset }) => {
+  return goods.map(good => (
+    <Item
+      good={good}
+      selectedGood={selectedGood}
+      setSelectedGood={setSelectedGood}
+      reset={reset}
+    />
+  ));
+};
+
+const Item = ({ good, selectedGood, setSelectedGood, reset }) => (
+  <tr
+    data-cy="Good"
+    className={selectedGood === good ? `has-background-success-light` : ``}
+    key={good}
+  >
+    <td>
+      {selectedGood !== good ? (
+        <button
+          data-cy="AddButton"
+          type="button"
+          className="button"
+          onClick={() => {
+            setSelectedGood(good);
+          }}
+        >
+          +
+        </button>
+      ) : (
+        <button
+          data-cy="RemoveButton"
+          type="button"
+          className="button is-info"
+          onClick={reset}
+        >
+          -
+        </button>
+      )}
+    </td>
+
+    <td data-cy="GoodTitle" className="is-vcentered">
+      {good}
+    </td>
+  </tr>
+);
+
 export const App = () => {
   const [selectedGood, setSelectedGood] = useState('Jam');
+  const reset = () => setSelectedGood('');
 
   return (
     <main className="section container">
@@ -25,60 +85,16 @@ export const App = () => {
           No goods selected
         </h1>
       ) : (
-        <h1 className="title is-flex is-align-items-center">
-          {selectedGood} is selected
-          <button
-            data-cy="ClearButton"
-            type="button"
-            className="delete ml-3"
-            onClick={() => {
-              setSelectedGood('');
-            }}
-          />
-        </h1>
+        <SelectedGood selectedGood={selectedGood} reset={reset} />
       )}
 
       <table className="table">
         <tbody>
-          {goods.map(good => (
-            <tr
-              data-cy="Good"
-              className={
-                selectedGood === good ? `has-background-success-light` : ``
-              }
-              key={good}
-            >
-              <td>
-                {selectedGood !== good ? (
-                  <button
-                    data-cy="AddButton"
-                    type="button"
-                    className="button"
-                    onClick={() => {
-                      setSelectedGood(good);
-                    }}
-                  >
-                    +
-                  </button>
-                ) : (
-                  <button
-                    data-cy="RemoveButton"
-                    type="button"
-                    className="button is-info"
-                    onClick={() => {
-                      setSelectedGood('');
-                    }}
-                  >
-                    -
-                  </button>
-                )}
-              </td>
-
-              <td data-cy="GoodTitle" className="is-vcentered">
-                {good}
-              </td>
-            </tr>
-          ))}
+          <Table
+            selectedGood={selectedGood}
+            setSelectedGood={setSelectedGood}
+            reset={reset}
+          />
         </tbody>
       </table>
     </main>
